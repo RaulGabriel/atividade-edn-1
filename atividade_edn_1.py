4f0d8cdd7ade4fed608da034183a015d689a4cd4
RaulGabriel <raulgabrielcardoso98@gmail.com>
2025-07-24 19:49:27 -0400

    Atividade 01 - EDN

---
+++ atividade_edn_1.py
@@ -0,0 +1,37 @@
+#==========================================================================
+# ATIVIDADE PRÁTICA 01 - EDN
+#==========================================================================
+
+# 1) Programa de Saudação
+
+print("Olá, mundo!")
+
+# 2) Calculadora de Soma
+
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print(f"O resultado da soma de {numero1} com {numero2} é: {soma}")
+
+# 3) Calculadora de Volume
+
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento*largura*altura
+
+print(f"O volume da caixa é: {volume} cm³")
+
+
+# 4) Calculadora de Preço total
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+qtde = 3
+total = preco_unitario*qtde
+
+print("=== Detalhes da Compra ===")
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: {preco_unitario}")
+print("Quantidade:", qtde)
+print(f"Total: {total}")
